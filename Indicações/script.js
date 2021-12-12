3bf25e4b70e947b91291918db134e21f9b20697c
Marina Lopes Rodrigues <72106436+Chernomari@users.noreply.github.com>
2021-12-12 18:20:50 -0300

    Add files via upload

---
+++ Indicações/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {  
+
+let mensagem, resposta, mensagem2;
+mensagem = prompt('Olá, pessoa! Você se importaria em responder algumas perguntas sobre esse site? (Favor responder com "sim" ou "não" e se você já tiver respondido, favor escrever "não"): ');
+resposta = (`${mensagem}`);
+if (resposta == 'sim') {
+    window.location.href = "https://forms.gle/M7wVhRuLRRh6a8TK8";
+} else {
+    mensagem2 = alert('Ah sim! Agradeço de qualquer forma!')
+}
+}
