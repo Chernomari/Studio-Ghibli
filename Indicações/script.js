2e55f4036d91284a5a1bb75f155624721e58891a
Marina Lopes Rodrigues <72106436+Chernomari@users.noreply.github.com>
2021-12-12 19:23:34 -0300

    Update script.js

--- Indicações/script.js
+++ Indicações/script.js
@@ -1,7 +1,7 @@
 window.onload = function () {  
 
 let mensagem, resposta, mensagem2;
-mensagem = prompt('Olá, pessoa! Você se importaria em responder algumas perguntas sobre esse site? (Favor responder com "sim" ou "não" e se você já tiver respondido, favor escrever "não"): ');
+mensagem = prompt('Olá, pessoa! Você gostaria de responder a algumas perguntas sobre esse site? (Favor responder com "sim" ou "não" e se você já tiver respondido, favor escrever "não"): ');
 resposta = (`${mensagem}`);
 if (resposta == 'sim') {
     window.location.href = "https://forms.gle/M7wVhRuLRRh6a8TK8";
